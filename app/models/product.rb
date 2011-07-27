92c52a44ca214fad8fa68645a30998eddab2fc0e
bazhongjie <bazj90217@sina.com>
2011-07-27 17:26:24 +0800

    finish checkout,Closes gh-5

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ validates :image_url, :format =>{
 }
 default_scope :order => 'title'
 has_many :line_items
+has_many :orders, :through => :line_items
 
 before_destroy :ensure_not_referenced_by_any_line_item
 
