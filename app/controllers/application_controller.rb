6ac9c8f78d50a7247b94fe9d0e9e9143d92c22ed
zhuhaofeng <375633799@qq.com>
2011-07-29 21:54:19 +0800

    fix the broken project in usercontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     end
    protected
      def authorize
-        unless User.find_by_id(session[:User_id])
+        unless User.find_by_id(session[:user_id])
          redirect_to login_url,:notice=>"Please log in"
         end
      end
