76d44f5807ba99ffc5f5352c16ac05e3ef000cfa
zhuhaofeng <375633799@qq.com>
2011-07-29 17:10:30 +0800

    fix errors in test_helper for login redirections

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,14 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+    
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
   def login_as(user)
     session[:user_id]=users(user).id
   end
@@ -12,12 +20,4 @@ class ActiveSupport::TestCase
   def setup
     login_as :one if defined?session
   end
-  
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
