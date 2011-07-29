a590ea720f259c7858224abeef8d2f2ece7db382
zhuhaofeng <375633799@qq.com>
2011-07-29 17:34:18 +0800

    review User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,12 @@ class User < ActiveRecord::Base
   attr_reader :password
   validate :password_must_be_present
   class<<self
-    def authenticate(name,password)
+    def User.authenticate(name,password)
       if user=find_by_name(name)
         if user.hashed_password==encrypt_password(password,user.salt)
           user
          end
       end
-    end
-    def encrypt_password(password,salt)
-    Digest::SHA2.hexdigest(password+"wibble"+salt)
     end
   end
   def ensure_an_admin_remains
@@ -38,4 +35,8 @@ class User < ActiveRecord::Base
      self.salt=self.object_id.to_s+rand.to_s
       end
     end 
+    def User.encrypt_password(password,salt)
+       Digest::SHA2.hexdigest(password+"wibble"+salt)
+    end
+
 
