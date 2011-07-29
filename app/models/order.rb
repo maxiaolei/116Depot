74821d47ee86ecafddb1aeeaf83bbf8eec70f778
zhuhaofeng <375633799@qq.com>
2011-07-29 22:45:05 +0800

    add email validating,change the UI in orders,add notice show in the login page

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,10 @@ class Order < ActiveRecord::Base
   PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
   
   validates :name, :address, :email, :pay_type, :presence => true
+  validates :email, :format => {
+    :with    => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+    :message => "The email address is invalid!"
+  }
   validates :pay_type, :inclusion => PAYMENT_TYPES
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
