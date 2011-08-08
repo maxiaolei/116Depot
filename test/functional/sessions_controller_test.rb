f16b7dc8426c9d812706c6552c3e621d9dc73e12
zhuhaofeng <375633799@qq.com>
2011-08-08 10:39:13 +0800

    fixed functional test

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -9,7 +9,7 @@ class SessionsControllerTest < ActionController::TestCase
    test "should login" do
      dave=users(:one)
      post:create,:name=>dave.name,:password=>'secret'
-     assert_redirected_to admin_url
+     assert_redirected_to store_url
      assert_equal dave.id, session[:user_id]
    end
    test "should fail login" do
