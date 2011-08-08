8d136a76f7e9ba956e8031cd97f5bdfc6ff40243
zhuhaofeng <375633799@qq.com>
2011-08-08 10:40:12 +0800

    fixed functional test

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -6,7 +6,7 @@ class StoreControllerTest < ActionController::TestCase
     assert_response :success
     assert_select '#columns #side a', :minimum => 4
     assert_select '#main .entry', 3
-    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select 'h3', 'Title: Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
