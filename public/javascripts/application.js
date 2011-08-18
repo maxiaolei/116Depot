4678b2049314edb6deaf5192a529cb904539b9fb
zhuhaofeng <375633799@qq.com>
2011-08-18 08:58:31 +0800

    fix the javascript conflicts in store#index

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,14 +2,8 @@
 // This file is automatically included by javascript_include_tag :defaults
 window.onload=function(){
 	try{
-		for(var i=0;i<id.length;i++){
-			$(id.pop()).raty({readOnly:true,path:'../../images',start:level.pop()});
-		};
-	}catch(e){
-		try{
-			$('#star_read').raty({readOnly:true,path:'../../images',start:product_level});
-			}catch(e){
-		}
+		$('#star_read').raty({readOnly:true,path:'../../images',start:product_level});
+		}catch(e){		
 	}
  	try{
 		$('#star_edit').raty({path:'../../images',start:product_level,});					
